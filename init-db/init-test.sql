1f80c98e79382b8f287c5e92008197b0fb14ceb2
Trigyi <82100202+Trigyi@users.noreply.github.com>
2025-06-30 14:19:12 +0200

    change init-db

--- init-db/init-test.sql
+++ init-db/init-test.sql
@@ -1,6 +1,6 @@
-CREATE ROLE app_user WITH LOGIN PASSWORD 'app_password';
+CREATE ROLE todo WITH LOGIN PASSWORD 'todoPWD';
 
-CREATE DATABASE app_db_test
-    WITH OWNER = app_user
+CREATE DATABASE laura
+    WITH OWNER = todo
     ENCODING = 'UTF8'
     TEMPLATE = template0;
